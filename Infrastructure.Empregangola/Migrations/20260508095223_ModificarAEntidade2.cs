f28a7b4c12a3dee137544c217f65d44c5a0416d4
cigano <liplight@gmail.com>
2026-05-08 11:27:46 +0100

    Melhorias na tabela detalhes do utilizador

---
+++ Infrastructure.Empregangola/Migrations/20260508095223_ModificarAEntidade2.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Empregangola.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModificarAEntidade2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Telefone",
+                table: "UserDetails",
+                newName: "PhoneNumber");
+
+            migrationBuilder.RenameColumn(
+                name: "Code",
+                table: "UserDetails",
+                newName: "DialCode");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PhoneNumber",
+                table: "UserDetails",
+                newName: "Telefone");
+
+            migrationBuilder.RenameColumn(
+                name: "DialCode",
+                table: "UserDetails",
+                newName: "Code");
+        }
+    }
+}
--- Infrastructure.Empregangola/Migrations/20260508100828_ModificarAEntidade3.Designer.cs
+++ Infrastructure.Empregangola/Migrations/20260508100828_ModificarAEntidade3.Designer.cs
@@ -4,6 +4,7 @@ using Infrastructure.Empregangola.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.Empregangola.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260508100828_ModificarAEntidade3")]
+    partial class ModificarAEntidade3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -119,10 +122,6 @@ namespace Infrastructure.Empregangola.Migrations
                         .HasColumnType("datetime2");
 
                     b.Property<string>("Location")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<string>("PhotoPrifile")
                         .IsRequired()
                         .HasColumnType("nvarchar(max)");
 
